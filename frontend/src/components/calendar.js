dc1b4a244290f1d37432142cc993dac80680e250
toestedbwead <leielcano@gmail.com>
2026-02-15 23:59:26 +0800

    rendered my first react component

---
+++ frontend/src/components/calendar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Calendar() {
+    return(
+        <div>
+            <h1>February 2026</h1>
+            <p>Calendar goes here</p>
+        </div>
+    )
+}
+
+export default Calendar
