51abf8e8647cc25c371cf1058b483f9fd372dd61
toestedbwead <leielcano@gmail.com>
2026-02-15 14:13:51 +0800

    Done with User and Entry models.

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# Create your models here.
+# User Model
+class User(AbstractUser):
+    pass
+
+# Entry Model
+
+class Entry(models.Model):
+    date = models.DateField()
+    description = models.CharField(max_length=200)
+    reflections = models.CharField(max_length=500, blank=True, null=True)
+    mood = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    category = models.CharField(
+        max_length = 30,
+        choices=[
+            ('coding', 'Coding'),
+            ('household_chores', 'Household Chores'),
+            ('outside_errands', 'Outside Errands'),
+            ('hangouts', 'Hangouts'),
+            ('gaming', 'Gaming')
+        ]
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  
+    created_at = models.DateTimeField(auto_now_add=True) 
